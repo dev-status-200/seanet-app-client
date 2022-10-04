64973fdc5230f1c87265fd65dd5bba07a96141ba
DESKTOP-GN1I4I1\HPP <syedabdullahteamhail@gmail.com>
2022-10-04 15:23:40 +0500

    updated

--- components/Layouts/MapComp.js
+++ components/Layouts/MapComp.js
@@ -1,60 +1,84 @@
-import React, { useState, useEffect, useRef } from 'react'
-import mapboxgl from 'mapbox-gl'
+import React, { useState, useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+import io from "socket.io-client";
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWJkdWxsYWh0ZWFtaGFpbCIsImEiOiJjbDdvbGtucjEwNm91M3ZueGZwaTEwcDg4In0.Be48QvgVjJ5-MNt3pzEnfw'
+const socket = io.connect(process.env.NEXT_PUBLIC_SEANET_SYS_URL);
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWJkdWxsYWh0ZWFtaGFpbCIsImEiOiJjbDdvbGtucjEwNm91M3ZueGZwaTEwcDg4In0.Be48QvgVjJ5-MNt3pzEnfw';
 
 function MapComp() {
   const mapContainer = useRef();
 
+  const [long, setLong] = useState('');
+  const [lat, setLat] = useState('');
+
   useEffect(() => {
-    const map = new mapboxgl.Map({
-      container:mapContainer.current,
-      style: 'mapbox://styles/mapbox/dark-v10',
-      center:[
-        -122.015807,
-        45.655529
-    ],
-      zoom: 15
-    })
-    map.on('load', async () => {
-		const response = await fetch('https://docs.mapbox.com/mapbox-gl-js/assets/hike.geojson');
-		const data = await response.json();
-		const coordinates = data.features[0].geometry.coordinates;
-
-		data.features[0].geometry.coordinates = [coordinates[0]];
-
-		map.addSource('trace', { type: 'geojson', data: data });
-		map.addLayer({
-			'id': 'trace',
-			'type': 'line',
-			'source': 'trace',
-			'paint': {
-				'line-color': 'yellow',
-				'line-opacity': 0.75,
-				'line-width': 5
-			}
-		});
-		// setup the viewport
-		let i = 0;
-		const timer = setInterval(() => {
-			if (i < coordinates.length) {
-				data.features[0].geometry.coordinates.push(coordinates[i]);
-				map.getSource('trace').setData(data);
-				map.panTo(coordinates[i]);
-				i++;
-			} else {
-				window.clearInterval(timer);
-			}
-		}, 10);
+	const map = new mapboxgl.Map({
+		container:mapContainer.current,
+		style: 'mapbox://styles/mapbox/dark-v10',
+		center:[
+			67.06765895742515,
+			24.877320553480782
+	  ],
+		zoom: 15
+	  })
+	  map.on('load', async () => {
+		  let data = {
+			type: "FeatureCollection",
+			features: [
+				{
+					type: "Feature",
+					geometry: {
+						type: "LineString",
+						coordinates: [
+							[
+								67.06765895742515,
+								24.877320553480782
+							],
+						]
+					}
+				}
+			]
 		}
-	);
-  })
-  
+		  map.addSource('trace', { type: 'geojson', data: data });
+		  map.addLayer({
+			  'id': 'trace',
+			  'type': 'line',
+			  'source': 'trace',
+			  'paint': {
+				  'line-color': 'yellow',
+				  'line-opacity': 0.75,
+				  'line-width': 3
+			  }
+		  });
+
+		  let coords = []
+		  socket.on("receive_message", (datas) => {
+			console.log(parseFloat(datas.long), parseFloat(datas.lat));
+			console.log(datas.long, datas.lat);
+				 coords = [ parseFloat(datas.long), parseFloat(datas.lat) ]
+
+				 data.features[0].geometry.coordinates.push(coords);
+				 map.getSource('trace').setData(data);
+				 map.panTo(coords);
+		  });
+		}
+	  );
+
+	},[socket])
+
+	const sendMessage = () => {
+	  socket.emit("send_message", {lat:lat, long:long});
+	};
 
   return (
     <div className='box'>
       <div ref={mapContainer} style={{width:'80vw', height:'70vh', borderRadius:5, border:'1px solid black'}}></div>
-    </div>
+	<input value={lat} onChange={(e)=>setLat(e.target.value)} placeholder='lattitude' />
+	<input value={long} onChange={(e)=>setLong(e.target.value)} placeholder='longitude' />
+	<button onClick={sendMessage}>Click</button>
+	</div>
   )
 }
+
 export default MapComp
